3310c57c8f9f1689d0b751c0feb2c3397a87d62c
Akash Kumar <Akodiyaakash@gmail.com>
2021-12-30 09:53:36 +0530

    Rotate

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rotate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+                int[] a = new int[100];
+                Console.WriteLine("Elements aof array: ");
+                for (int i = 0; i < a.Length; i++)
+                {
+                    a[i] = int.Parse(Console.ReadLine());
+                }
+
+                int temp;
+                for (int j = 0; j < a.Length - 1; j++)
+                {
+                    temp = a[0];
+                    a[0] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+                Console.WriteLine("Rotated Elements: ");
+                foreach (int num in a)
+                {
+                    Console.Write(num + " ");
+                }
+                Console.ReadKey();
+        }
+    }
+}
